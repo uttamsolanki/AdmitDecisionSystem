1ae585b6064fcf83e191945ea5435578788fd94d
Vasu Shah <shah18b@uwindsor.ca>
2019-07-25 02:23:10 -0400

    Student Add-Update

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -5,11 +5,17 @@ import { IconsComponent } from '../../pages/icons/icons.component';
 import { MapsComponent } from '../../pages/maps/maps.component';
 import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
 import { TablesComponent } from '../../pages/tables/tables.component';
+import { StudentsComponent } from '../../pages/students/students.component';
+import { AddUpdateStudentComponent } from '../../pages/students/add-update/add-update.component';
+import { ViewStudentComponent } from '../../pages/students/view/view.component';
 
 export const AdminLayoutRoutes: Routes = [
     { path: 'dashboard',      component: DashboardComponent },
     { path: 'user-profile',   component: UserProfileComponent },
     { path: 'tables',         component: TablesComponent },
     { path: 'icons',          component: IconsComponent },
-    { path: 'maps',           component: MapsComponent }
+    { path: 'maps',           component: MapsComponent },
+    { path: 'students',       component: StudentsComponent },
+    { path: 'students/addupdate',	component: AddUpdateStudentComponent },
+    { path: 'students/view',	component: ViewStudentComponent },
 ];
---
+++ src/app/pages/students/add-update/add-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-update',
+  templateUrl: './add-update.component.html',
+  styleUrls: ['./add-update.component.scss']
+})
+export class AddUpdateStudentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
