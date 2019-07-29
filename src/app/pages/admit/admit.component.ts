f547ed263140dff3eb5cc7b571e03b9a0ee5e631
Uttam Solanki <kanudo@Uttams-MacBook-Pro.local>
2019-07-29 01:58:07 -0400

    added charts

--- src/app/pages/admit/admit.component.ts
+++ src/app/pages/admit/admit.component.ts
@@ -56,7 +56,6 @@ export class AdmitComponent implements OnInit {
   get f() { return this.admitForm.controls; }
 
   getUniversityData(){
-    this.searched = true;
 
     this.datasets = [
       [0, 20, 10, 30, 15, 40, 20, 60, 60],
@@ -64,14 +63,25 @@ export class AdmitComponent implements OnInit {
     ];
     this.data = this.datasets[0];
 
+
+    var chartOrders = document.getElementById('chart-orders');
+
+    parseOptions(Chart, chartOptions());
+
+
+    var ordersChart = new Chart(chartOrders, {
+      type: 'bar',
+      options: chartExample2.options,
+      data: chartExample2.data
+    });
+
     var chartSales = document.getElementById('chart-sales');
 
     this.salesChart = new Chart(chartSales, {
-			type: 'line',
-			options: chartExample1.options,
-			data: chartExample1.data
-		});
-
+      type: 'line',
+      options: chartExample1.options,
+      data: chartExample1.data
+    });
     this.updateOptions();
   }
   public updateOptions() {
