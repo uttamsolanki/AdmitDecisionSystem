6b8c1d2b3571c975324c08398022edc831e1b798
Vasu Shah <shah18b@uwindsor.ca>
2019-07-29 02:28:56 -0400

    Search by last name added

--- src/app/pages/students/filter.pipe.ts
+++ src/app/pages/students/filter.pipe.ts
@@ -9,7 +9,7 @@ export class filterPipe implements  PipeTransform{
         if(!students || !search){
             return students;
         }
-        return students.filter(student=>student.fname.toLowerCase().indexOf(search.toLowerCase())!==-1);
+        return students.filter(student=>student.fname.toLowerCase().indexOf(search.toLowerCase())!==-1 || student.lname.toLowerCase().indexOf(search.toLowerCase())!==-1);
     }
     
 }
